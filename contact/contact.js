03613d61b1b67e3fef132aa5268e1df65ebc9345
peraden <petarerste@hotmail.com>
2023-02-08 13:08:16 +0100

    created contact.html with js form submission

---
+++ contact/contact.js
@@ -0,0 +1,66 @@
+var fields = {};
+
+document.addEventListener("DOMContentLoaded", function() {
+    fields.firstName = document.getElementById('firstName');
+    fields.lastName = document.getElementById('lastName');
+    fields.email = document.getElementById('email');
+    fields.question = document.getElementById('question');
+   })
+
+   function isNotEmpty(value) {
+    if (value == null || typeof value == 'undefined' ) return false;
+    return (value.length > 0);
+   }
+
+   function isEmail(email) {
+    let regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+    return regex.test(String(email).toLowerCase());
+   }
+
+   function fieldValidation(field, validationFunction) {
+    if (field == null) return false;
+   
+    let isFieldValid = validationFunction(field.value)
+    if (!isFieldValid) {
+    field.className = 'placeholderRed';
+    } else {
+    field.className = '';
+    }
+   
+    return isFieldValid;
+   }
+
+   function isValid() {
+    var valid = true;
+    
+    valid &= fieldValidation(fields.firstName, isNotEmpty);
+    valid &= fieldValidation(fields.lastName, isNotEmpty);
+    valid &= fieldValidation(fields.email, isEmail);
+    valid &= fieldValidation(fields.question, isNotEmpty);
+
+    return valid;
+   }
+   
+   class User {
+    constructor(firstName, lastName, email, question) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.question = question;
+    }
+   }
+
+   function sendContact() {
+    if (isValid()) {
+        let usr = new User(firstName.value, lastName.value, email.value, question.value);
+
+        alert(`${usr.firstName}, thanks for registering.`);
+        console.log(usr);
+    } else {
+        alert("There was an error.");
+    }
+   }
+
+
+
+   
